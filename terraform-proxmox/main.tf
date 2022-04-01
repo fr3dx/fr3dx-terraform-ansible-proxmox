39df901b94a59f2e5388a7be39bc0ed37e55816b
fr3dx <ferenc.m@protonmail.com>
2022-04-01 13:11:31 +0000

    v1

---
+++ terraform-proxmox/main.tf
@@ -0,0 +1,63 @@
+provider "proxmox" {
+  pm_api_url          = var.api_url
+  pm_api_token_id     = var.api_token_id
+  pm_api_token_secret = var.api_token_secret
+  pm_tls_insecure     = true
+}
+
+resource "proxmox_vm_qemu" "cloudinit-test" {
+  count       = 1
+  name        = "tf-vm-${count.index + 1}"
+  target_node = var.proxmox_host
+  clone       = var.template_name
+  agent       = 0
+  os_type     = "cloud-init"
+  cores       = 2
+  sockets     = 1
+  cpu         = "host"
+  memory      = 2048
+  scsihw      = "virtio-scsi-pci"
+  bootdisk    = "scsi0"
+  bios        = "seabios"
+  disk {
+    slot    = 0
+    size    = "20G"
+    type    = "virtio"
+    storage = "STORAGE"
+  }
+
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      network,
+    ]
+  }
+
+  ipconfig0 = "ip=${var.ips[count.index]}/24,gw=${cidrhost(format("%s/24", var.ips[count.index]), 1)}"
+
+  sshkeys = <<EOF
+  ${var.ssh_key}
+  EOF
+
+  connection {
+    host        = var.ips[count.index]
+    user        = var.user
+    private_key = file(var.ssh_keys["priv"])
+    agent       = false
+    timeout     = "3m"
+  }
+
+  provisioner "remote-exec" {
+    inline = ["echo 'SSH test OK'"]
+  }
+
+  provisioner "local-exec" {
+    working_dir = "../terraform-ansible"
+    command     = "ansible-playbook -u ${var.user} --key-file ${var.ssh_keys["priv"]} -i ${var.ips[count.index]}, main.yml"
+  }
+
+}
